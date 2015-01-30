48b9a907c32e9870094949c6d544448211d2c4dd
Shouyuan Huang <huang706@wind.ecn.purdue.edu>
2015-01-30 11:20:14 -0500

    strcat_ex pass

--- pa03/answer03.c
+++ pa03/answer03.c
@@ -1,21 +1,27 @@
-#ifndef PA03_H
-#define PA03_H
-
 #include <stdlib.h>
 #include <string.h>
 #include "answer03.h"
 
 char * strcat_ex(char ** dest, int * n, const char * src)
 {
-  size_t dest_len = 0, src_len = strlen(src);
-  if( (*dest == NULL) || ((dest_len = strlen(*dest)) + src_len + 1 > *n) )
+  if(*dest == NULL)
     {
-      int N = 1 + (dest_len + src_len) * 2;
-      *n = N;
-      char * buffer = malloc(N * sizeof(char));
-      buffer = strcpy(buffer, (const char*) *dest);
-      free(*dest);
-      dest = &buffer;
+      *n = strlen(src) * 2 + 1;
+      char * buffer = (char *) malloc(*n);
+      /* Pleas be very careful: If *dest is null. It can't be copied or concatenated. Thus it is very non-intuitive to write these two cases together.  */
+      *dest = buffer;
+    }
+  else
+    {
+      size_t N = 1 + (strlen(*dest) + strlen(src)) * 2;
+      if(N+1>*n)
+	{
+	  *n = N;
+	  char * buffer = (char*) malloc(N);
+	  buffer = strcpy(buffer, (const char*) *dest);
+	  free(*dest);
+	  *dest = buffer;
+	}
     }
   *dest = strcat(*dest, (char*)src);
   return *dest;
@@ -26,7 +32,7 @@ char * strcat_ex(char ** dest, int * n, const char * src)
 char * * explode(const char * str, const char * delims, int * arrLen)
 {
   
-  int * delimPos;
+  int delimPos[*arrLen];
   int ind;
   int N = 0;
   for(ind = 0; ind < *arrLen; ind++)
@@ -43,7 +49,9 @@ char * * explode(const char * str, const char * delims, int * arrLen)
       strArr[ind+1] = &(newArr[delimPos[ind]+1]);
     }
   return strArr;
+  
 }
+
 
 char * implode(char * * strArr, int len, const char * glue)
 {
@@ -61,4 +69,4 @@ void destroyStringArray(char * * strArr, int len)
 {
   
 }
-#endif
+
